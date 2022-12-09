84e6fb809289c10737fad5b20ebc7f1268e68316
jmlago <josemlagoalonso@gmail.com>
2022-12-09 21:17:20 +0000

    repo init

---
+++ jmlago-page/src/components/Welcome.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="centered">
+  <div class="greetings">
+    <h1 class="orange1">{{ msg }}</h1>
+    <h3 class="purple1">
+      I'm 
+      <a class="orange1" href="https://twitter.com/josemlago" target="_blank" rel="noopener">@josemlago</a>,
+       a micro-freelancer and indie hacker!
+    </h3>
+    <h2 class="purple1">
+      Select with arrows and press enter
+    </h2>
+  </div>
+  <div class="menu">
+    <RouterLink class="orange1 menu" to="/gigs">Gigs that I offer</RouterLink>
+  </div>
+  <div class="menu">
+    <RouterLink class="purple1 menu" to="/projects">Projects that I'm involved</RouterLink>
+  </div>
+  <div class="menu">
+    <RouterLink class="purple1 menu" to="/mystory">My story</RouterLink>
+  </div>
+</div>
+</template>
+
+<style scoped>
+h1 {
+  font-family: "broken-console-bold";
+  font-size: 7rem;
+}
+
+h3 {
+  font-family: "broken-console";
+  font-size: 2rem;
+}
+h2 {
+  font-family: "broken-console-bold";
+  font-size: 3rem;
+  top: 10px;
+}
+
+.menu {
+  text-align: center;
+  font-family: "broken-console";
+  font-size: 2rem;
+  top: 20px;
+}
+.menu div{
+  text-align: center;
+}
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+.greetings h2 {
+  text-align: center;
+}
+
+.centered {
+  top: 60%;
+  text-align: center;
+}
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: center;
+  }
+  .greetings h2 {
+    text-align: center;
+  }
+}
+</style>
---
+++ jmlago-page/src/views/GigsView.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 id="gigTitle">Gigs that I offer</h1>
+    <p>I’m a micro-freelancer. So what does it really mean?</p>
+    <p>I try only to do small projects, ranging from 50 to 1000$, with just take at most two weeks to complete.</p> 
+    <p>But why? Is the ONLY WAY to build trust little by little, aligning reality and expectations from the first beginning. Moreover, removes almost all communication issues.</p>
+    <p>In exceptional situations I can do longer projects, but always divided in small milestones with the same ranges. But this is not a usual setting.</p>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+p{
+  font-size: 16px;
+}
+  #gigTitle{
+  font-family: "broken-console-bold";
+  font-size: 30px;
+  color: greenyellow;
+}
+}
+
+</style>
